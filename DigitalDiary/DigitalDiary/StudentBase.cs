79bd764445bf690cb88ee84b7c4ede86a58db3c2
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-12-19 17:03:03 +0100

    Add class StudentBase

---
+++ DigitalDiary/DigitalDiary/StudentBase.cs
@@ -0,0 +1,13 @@
+﻿namespace DigitalDiary
+{
+    public abstract class StudentBase : IStudent
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public abstract void AddGrade();
+        public abstract void RemoveGrade();
+
+        public abstract Statistics GetStatistics();
+    }
+}
