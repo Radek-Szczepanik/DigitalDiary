73169adbe07d36ea485c217c278613354392ab4c
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2021-12-19 16:55:47 +0100

    Add class Statistics

--- DigitalDiary/DigitalDiary/Statistics.cs
+++ DigitalDiary/DigitalDiary/Statistics.cs
@@ -1,7 +1,36 @@
-﻿namespace DigitalDiary
+﻿using System;
+
+namespace DigitalDiary
 {
     public class Statistics
     {
-        
+        private double Min;
+        private double Max;
+        private double Sum;
+        private int Count;
+
+        public Statistics()
+        {
+            Min = double.MinValue;
+            Max = double.MaxValue;
+            Count = 0;
+            Sum = 0.0;
+        }
+
+        public double Average
+        {
+            get
+            {
+                return Sum / Count;
+            }
+        }
+
+        public void Add(double number)
+        {
+            Sum += number;
+            Count++;
+            Min = Math.Min(Max, number);
+            Max = Math.Max(Min, number);
+        }
     }
 }
